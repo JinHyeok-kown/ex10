b13fd8051ac9c931c929e73dc0f39cec04c4c8f6
USER <USER@USER5-PC>
2018-09-27 14:15:51 +0900

    오류

---
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -0,0 +1,14 @@
+package kr.itedu.ex10.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
